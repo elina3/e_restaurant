b62f1a48352e92c3d2f9420ac1b246d655f674c7
elina3 <elina@zhuzhuqs.com>
2016-05-06 00:12:10 +0800

    Add card filter

--- manager_web/www/controllers/users/card_history.client.controller.js
+++ manager_web/www/controllers/users/card_history.client.controller.js
@@ -21,6 +21,29 @@ angular.module('EWeb').controller('CardHistoryController',
         cardHistories: []
       };
 
+      $scope.pageShow = {
+        createTimeRange: '',
+        createTimeMinTime: moment().format('YY/MM/DD HH:mm'),
+        dateOptions: {
+          locale: {
+            fromLabel: '起始时间',
+            toLabel: '结束时间',
+            cancelLabel: '取消',
+            applyLabel: '确定',
+            customRangeLabel: '区间',
+            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
+            firstDay: 1,
+            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月',
+              '十月', '十一月', '十二月']
+          },
+          timePicker: true,
+          timePicker12Hour: false,
+          timePickerIncrement: 1,
+          separator: ' ~ ',
+          format: 'YY/MM/DD HH:mm'
+        }
+      };
+
 
       $scope.searchKey = function(){
         loadCardHistories();
