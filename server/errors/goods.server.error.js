f1706953a9df4a155c2caa5bcf16cd9238eacc58
elina3 <elina@zhuzhuqs.com>
2016-05-07 19:06:02 +0800

    Add zh_message

--- server/errors/goods.server.error.js
+++ server/errors/goods.server.error.js
@@ -6,9 +6,9 @@
 var _ = require('lodash');
 
 module.exports = _.extend(exports, {
-  goods_not_exist: {type: 'goods_not_exist', message: 'This goods is not exist!'},
-  goods_deleted: {type: 'goods_deleted', message: 'This goods has been deleted'},
-  goods_name_null: {type: 'goods_name_null', message: 'The goods name is null!'},
-  goods_price_null: {type: 'goods_price_null', message: 'The goods has no price'},
-  goods_id_null: {type: 'goods_id_null', message: 'The goods id is null'}
+  goods_not_exist: {type: 'goods_not_exist', message: 'This goods is not exist!', zh_message: '商品不存在'},
+  goods_deleted: {type: 'goods_deleted', message: 'This goods has been deleted', zh_message: '商品已删除'},
+  goods_name_null: {type: 'goods_name_null', message: 'The goods name is null!', zh_message: '商品名称为空'},
+  goods_price_null: {type: 'goods_price_null', message: 'The goods has no price', zh_message: '商品价钱为空'},
+  goods_id_null: {type: 'goods_id_null', message: 'The goods id is null', zh_message: '商品id为空'}
 });
--- server/errors/order.server.error.js
+++ server/errors/order.server.error.js
@@ -10,12 +10,12 @@ module.exports = _.extend(exports, {
   order_deleted: {type: 'order_deleted', message: 'This order has been deleted', zh_message: '订单已删除！'},
   order_not_paid: {type: 'order_not_paid', message: 'The order is not paid!', zh_message: '订单未支付！'},
   order_paid: {type: 'order_paid', message: 'The order has been paid', zh_message: '订单已支付！'},
-  goods_cook_complete: {type: 'goods_cook_complete', message: 'The goods cook completed'},
-  goods_not_cook_complete: {type: 'goods_not_cook_complete', message: 'The goods not cook complete'},
-  no_goods_to_order: {type: 'no_goods_to_order', message: 'There is no goods to Order'},
-  goods_order_id_null_error: {type: 'goods_order_id_null_error', message: 'The goods order id is null'},
+  goods_cook_complete: {type: 'goods_cook_complete', message: 'The goods cook completed', zh_message: '商品烹饪完成'},
+  goods_not_cook_complete: {type: 'goods_not_cook_complete', message: 'The goods not cook complete', zh_message: '商品没有烹饪完成'},
+  no_goods_to_order: {type: 'no_goods_to_order', message: 'There is no goods to Order', zh_message: '没有任何商品需要下单'},
+  goods_order_id_null_error: {type: 'goods_order_id_null_error', message: 'The goods order id is null', zh_message: '订单商品id为空'},
   order_id_null_error: {type: 'order_id_null_error', message: 'The order id is null', zh_message: '订单id为空！'},
-  goods_order_not_exist: {type: 'goods_order_not_exist', message: 'The goods order is not exist'},
-  no_contact_info: {type: 'no_contact_info', message: 'There is no contact info!'},
-  uncompleted_contact: {type: 'uncompleted_contact', message: 'There is un completed contact!'}
+  goods_order_not_exist: {type: 'goods_order_not_exist', message: 'The goods order is not exist', zh_message: '商品订单不存在'},
+  no_contact_info: {type: 'no_contact_info', message: 'There is no contact info!', zh_message: '没有联系人信息'},
+  uncompleted_contact: {type: 'uncompleted_contact', message: 'There is un completed contact!', zh_message: '订单联系人不完整'}
 });
