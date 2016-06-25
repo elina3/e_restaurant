ce72557a296b6fbc3f47f6ecf61f506dd1e2bd7b
elina3 <elina@zhuzhuqs.com>
2016-06-25 23:47:38 +0800

    Complete the hospitalized_info view

--- manager_web/www/controllers/healthy_meal/hospitalized_info.client.controller.js
+++ manager_web/www/controllers/healthy_meal/hospitalized_info.client.controller.js
@@ -11,7 +11,7 @@ angular.module('EWeb').controller('HospitalizedInfoController',
       var sexes = [{id: 'female', text: '女'}, {id: 'male', text: '男'}, {id: 'unknown', text: '未知'}];
 
       $scope.panelConfig = {
-        isShow: true,
+        isShow: false,
         buildings: [],
         floors: [],
         beds: [],
@@ -45,12 +45,7 @@ angular.module('EWeb').controller('HospitalizedInfoController',
       };
 
       $scope.filter = {
-        currentBuilding: null,
-        currentFloor: null,
-        buildings: [],
-        floors: [],
-        beds: [],
-        sexes: [{id: 'female', text: '女'}, {id: 'male', text: '男'}, {id: 'unknown', text: '未知'}]
+        currentIdNumber: ''
       };
 
       function getFloorsByBuilding(building, callback){
@@ -83,79 +78,33 @@ angular.module('EWeb').controller('HospitalizedInfoController',
         });
       }
 
-      $scope.filterBuildingChange = function () {
-        if ($scope.filter.currentBuilding) {
-          getFloorsByBuilding($scope.filter.currentBuilding, function(err, floors){
-            if(!err){
-              $scope.filter.floors = floors;
-            }
-          });
-        }
-      };
-      $scope.filterFloorChange = function () {
-        if ($scope.filter.currentFloor) {
-          getBedsByFloor($scope.filter.currentFloor, function(err, beds){
-            if(!err && beds){
-              $scope.beds = beds;
-            }
-          });
-        }
-      };
-
       $scope.panelBuildingChange = function(){
-        if ($scope.pageConfig.newSicker.building) {
-          getFloorsByBuilding($scope.pageConfig.newSicker.building, function(err, floors){
+        if ($scope.panelConfig.newSicker.building) {
+          getFloorsByBuilding($scope.panelConfig.newSicker.building, function(err, floors){
             if(!err){
-              $scope.pageConfig.floors = floors;
+              $scope.panelConfig.floors = floors;
             }
           });
         }
       };
       $scope.panelFloorChange = function(){
-        if ($scope.pageConfig.newSicker.floor) {
-          getBedsByFloor($scope.pageConfig.newSicker.floor, function(err, beds){
+        if ($scope.panelConfig.newSicker.floor) {
+          getBedsByFloor($scope.panelConfig.newSicker.floor, function(err, beds){
             if(!err){
-              $scope.pageConfig.beds = beds;
+              $scope.panelConfig.beds = beds;
             }
           });
         }
       };
 
-      function getTimeStamp(){
-        if ($scope.pageShow.createTimeRange.startDate && $scope.pageShow.createTimeRange.startDate._d) {
-          var time = moment($scope.pageShow.createTimeRange.startDate);
-          return new Date(time).getTime();
-        }
-        return;
-      }
       $scope.search = function () {
-        var timeStamp = getTimeStamp();
-        if(!timeStamp){
-          return $scope.$emit(GlobalEvent.onShowAlert, '请选择时间！');
-        }
+        alert('hhh');
       };
-
-      $scope.datePicker = {
-        createTimeRange: '',
-        createTimeMinTime: moment().format('YY/MM/DD'),
-        dateOptions: {
-          locale: {
-            fromLabel: '起始时间',
-            toLabel: '结束时间',
-            cancelLabel: '取消',
-            applyLabel: '确定',
-            customRangeLabel: '区间',
-            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
-            firstDay: 1,
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月',
-              '十月', '十一月', '十二月']
-          },
-          timePicker: true,
-          timePicker12Hour: false,
-          timePickerIncrement: 1,
-          singleDatePicker: true,
-          format: 'YYYY/MM/DD HH:mm'
-        }
+      $scope.save = function(){
+        alert('save');
+      };
+      $scope.leaveHospital =function(){
+        alert('leave');
       };
 
       $scope.goBack = function () {
@@ -168,15 +117,12 @@ angular.module('EWeb').controller('HospitalizedInfoController',
             return {
               id: item._id,
               text: item.name
-            }
+            };
           });
-          $scope.filter.buildings = buildings;
           $scope.panelConfig.buildings = buildings;
 
           defaultBuilding = buildings[0];
-          $scope.filter.currentBuilding = defaultBuilding;
           $scope.panelConfig.newSicker.building = defaultBuilding;
-          $scope.filterBuildingChange();
           $scope.panelBuildingChange();
         }
       });
